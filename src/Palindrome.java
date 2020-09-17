4198decf5a2e2072ff875b9641e62a27f2ca12fb
A.A.R <aar@pop-os.localdomain>
2020-09-17 12:24:15 -0500

    Palindrome class

---
+++ src/Palindrome.java
@@ -0,0 +1,29 @@
+
+public class Palindrome {
+	
+	public static String reverse(String str) {
+		
+		String rev = "";
+		
+		for(int i = str.length()-1; i>=0; i--) {
+			rev += str.charAt(i);
+		}
+		
+		return rev;
+	}
+	
+	public static boolean isPalindrome(String str) {
+		
+		String rev = reverse(str);
+		System.out.println(rev);
+		
+		
+		return str.equalsIgnoreCase(rev);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(isPalindrome("raR"));
+	}
+
+}
